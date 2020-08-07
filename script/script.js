48748b531bf755681b2a5cdf113eea2159b4d779
Gennady Systerov <gena.systerov@yandex.ru>
2020-08-08 01:49:29 +0400

    закончил основную часть

--- script/script.js
+++ script/script.js
@@ -2,13 +2,17 @@ let money=60000
 console.log(typeof money)
 let income= 'создание сайтов'
 console.log(typeof income)
-var addExpenses='интернет, коммуналка, табак)))'
+var addExpenses='Интернет, Коммуналка, Табак)))'
 console.log(addExpenses.length);
 let deposit= Boolean(40)
 console.log(typeof deposit)
 let mission=50000
-console.log(mission)
-let period=11
+console.log('цель заработать '+ mission + ' рублей')
+let period=4
 console.log('период равен '+ period +' месяцев')
 alert("Heeeeeeeyyyyyoooo!!!");
+console.log(addExpenses.toLowerCase().split())
 console.log(2102200);
+console.log(addExpenses.split())
+let budgetDay= '30 тысяч в месяч доп дохода'
+console.log(budgetDay)
