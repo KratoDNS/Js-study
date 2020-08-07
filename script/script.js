a03f561e27c41cf97ed293e31cad9d9a12c90943
Gennady Systerov <gena.systerov@yandex.ru>
2020-08-08 00:59:27 +0400

    сделал 1 и часть 2 задания

--- script/script.js
+++ script/script.js
@@ -1,14 +1,14 @@
 let money=60000
-console.log(money)
+console.log(typeof money)
 let income= 'создание сайтов'
-console.log(income)
-let addExpenses='интернет, коммуналка, табак)))'
-console.log(addExpenses)
-let deposit= 40
-let deposit= true
-console.log(deposit)
+console.log(typeof income)
+var addExpenses='интернет, коммуналка, табак)))'
+console.log(addExpenses.length);
+let deposit= Boolean(40)
+console.log(typeof deposit)
 let mission=50000
 console.log(mission)
-let period=10
-alert("Heeeeeeeyyyyyoooo!!!");;
+let period=11
+console.log('период равен '+ period +' месяцев')
+alert("Heeeeeeeyyyyyoooo!!!");
 console.log(2102200);
